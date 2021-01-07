5f9c05bdf45683591f4828ddec4c6ad2e0227996
Soikat Rahman <s.mydevtoolsu@gmail.com>
2021-01-07 10:52:50 +0600

    Readme added

--- frontend/src/Pages/cart/CartScreen.js
+++ frontend/src/Pages/cart/CartScreen.js
@@ -60,8 +60,8 @@ export default function CartScreen() {
    const location = useLocation();
    const qty = location.search ? Number(location.search.split('=')[1]) : 1;
    const dispatch = useDispatch();
-   console.log(qty);
-   console.log(id);
+   // console.log(qty);
+   // console.log(id);
 
    useEffect(() => {
       if (id) dispatch(addToCart(id, qty));
